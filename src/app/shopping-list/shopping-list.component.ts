043b1688359c5b8ce947776e5bb4032a8cfee88c
Anna <akarachkovska@gmail.com>
2023-08-20 20:43:58 +0300

    Added a button for multiple ingredients removal

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { SelectionModel } from "@angular/cdk/collections";
-import { AfterViewInit, Component, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, ViewChild } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
@@ -13,7 +13,7 @@ import { ShoppingListService } from "./shopping-list.service";
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
-export class ShoppingListComponent implements OnInit, AfterViewInit {
+export class ShoppingListComponent implements AfterViewInit {
   dataSource: MatTableDataSource<Ingredient> = new MatTableDataSource();
   displayedColumns: string[] = ['select', 'position', 'name', 'amount', 'edit'];
   selection = new SelectionModel<Ingredient>(true, []);
@@ -32,8 +32,6 @@ export class ShoppingListComponent implements OnInit, AfterViewInit {
         this.editedItem = undefined;
       });
   }
-
-  ngOnInit() {}
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -54,20 +52,27 @@ export class ShoppingListComponent implements OnInit, AfterViewInit {
     this.selection.select(...this.dataSource.data);
   }
 
-  checkboxLabel(element?: Ingredient): string {
+  checkboxLabel(element?: Ingredient): string {    
     if (!element) {
       return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
     }
     return `${this.selection.isSelected(element) ? 'deselect' : 'select'} element ${this.dataSource.data.indexOf(element) + 2}`;
   }
 
-  onEditItem(name: string) {
+  editItem(name: string) {
     this.editedItem = this.shoppingListService.getIngredient(name);
     this.shoppingListService.startedEditing.next(name);
   }
 
-  onDeleteItem(name: string) {
+  deleteItem(name: string) {
     this.shoppingListService.deleteIngredient(name);
     this.shoppingListService.startedEditing.next('');
+  }
+
+  deleteAll() {
+    this.selection.selected.forEach(el => {
+      this.deleteItem(el.name);
+      this.selection.clear();
+    })
   }
 }
