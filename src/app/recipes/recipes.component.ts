be876abcf9ac0951c9e0fa3f26e722d0ef2c98b4
Anna <akarachkovska@gmail.com>
2023-09-14 09:59:16 +0300

    Add filter to meals page #15

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,9 +1,75 @@
-import { Component } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { map, Observable, startWith } from 'rxjs';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import { MealDbService } from 'app/shared/meal-db.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss']
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit{
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  tagControl = new FormControl('');
+  filteredResult: Observable<string[]>;
+  tags: string[] = [];
+  allTags: string[] = [];
+
+  @ViewChild('tagInput') tagInput: ElementRef<HTMLInputElement>;
+
+  announcer = inject(LiveAnnouncer);
+
+  constructor(private mealDbService: MealDbService) {
+    this.filteredResult = this.tagControl.valueChanges.pipe(
+      startWith(null),
+      map((tag: string | null) => (tag ? this._filter(tag) : this.allTags.slice())),
+    );
+  }
+
+  ngOnInit() {
+    this.mealDbService.getCategories().subscribe(
+      res => this.allTags = res
+    );
+    this.mealDbService.getAreas().subscribe(
+      res => this.allTags.push(...res)
+    )
+  }
+
+  add(event: MatChipInputEvent) {
+    const value = (event.value || '').trim();
+
+    if (value) {
+      this.tags.push(value);
+    }
+
+    event.chipInput!.clear();
+
+    this.tagControl.setValue(null);
+  }
+
+  remove(tag: string) {
+    const index = this.tags.indexOf(tag);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+
+      this.announcer.announce(`Removed ${tag}`);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent) {
+    this.tags.push(event.option.viewValue);
+    this.tagInput.nativeElement.value = '';
+    this.tagControl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allTags.filter(tag => tag.toLowerCase().includes(filterValue));
+  }
 }
