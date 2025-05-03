c896d0a578185fc9b1b6f76852e8d88fdc372ed0
Anna <akarachkovska@gmail.com>
2025-05-03 23:47:41 +0300

    update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,38 @@
 module.exports = function (config) {
   config.set({
-    browsers: ['ChromeHeadless'],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      clearContext: false,
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage'],
+      }
+    },
+    browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: true,
+    restartOnFileChange: false
   });
 };
